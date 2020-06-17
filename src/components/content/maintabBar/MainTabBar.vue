65158168cfc87949de7f8c9857c0d88e7a24e2c8
meiguangya <15871737166@163.com>
2020-06-17 11:07:48 +0800

    tabbar router注册

---
+++ src/components/content/maintabBar/MainTabBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <TabBar>
+    <TabBarItem link="/home">
+      <img slot="icon" src="~assets/img/tabbar/home.svg" alt="" />
+      <img slot="active-icon" src="~assets/img/tabbar/home_active.svg" alt="" />
+      <div slot="title">首页</div>
+    </TabBarItem>
+
+    <TabBarItem link="/category">
+      <img slot="icon" src="~assets/img/tabbar/category.svg" alt="" />
+      <img
+        slot="active-icon"
+        src="~assets/img/tabbar/category_active.svg"
+        alt=""
+      />
+      <div slot="title">分类</div>
+    </TabBarItem>
+
+    <TabBarItem link="/cart">
+      <img slot="icon" src="~assets/img/tabbar/cart.svg" alt="" />
+      <img slot="active-icon" src="~assets/img/tabbar/cart_active.svg" alt="" />
+      <div slot="title">购物车</div>
+    </TabBarItem>
+
+    <TabBarItem link="/profile">
+      <img slot="icon" src="~assets/img/tabbar/profile.svg" alt="" />
+      <img
+        slot="active-icon"
+        src="~assets/img/tabbar/profile_active.svg"
+        alt=""
+      />
+      <div slot="title">我的</div>
+    </TabBarItem>
+  </TabBar>
+</template>
+<script>
+import TabBar from "components/common/tabBar/TabBar";
+import TabBarItem from "components/common/tabBar/TabBarItem";
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem,
+  },
+};
+</script>
---
+++ src/rount/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Router from 'vue-router'
+import VueRouter from 'vue-router';
+
+Vue.use(Router);
+
+const routes = [];
+
+const router = new VueRouter({
+    mode:'history',
+    routes
+});
+
+export default router;
